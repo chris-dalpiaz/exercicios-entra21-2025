d1ffbef9505a386df5979e9cff781def82a153e4
CHRISTOFER EZEQUIEL DALPIAZ <christofer.dalpiaz@alunos.sc.senac.br>
2025-04-11 20:32:47 -0300

    Adicionando exercício 11

---
+++ exercicios-entra21-2025/src/Exercicio11.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio11 {
+    public static void main(String[] args) {
+        //11
+        ArrayList<Integer> listaInteiros = new ArrayList<>();
+        for (int i = 0; i <= 10; i++){
+            listaInteiros.add(i);
+        }
+        System.out.println(listaInteiros);
+    }
+}
